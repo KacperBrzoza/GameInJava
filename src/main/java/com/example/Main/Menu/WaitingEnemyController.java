547544786842e33a7e18e6d6b50d4f983a9a7c90
Kacper Brzezinski <kacperbrzoza@o2.pl>
2022-09-08 21:09:15 +0200

    Nowe karty

--- src/main/java/com/example/Main/Menu/WaitingEnemyController.java
+++ src/main/java/com/example/Main/Menu/WaitingEnemyController.java
@@ -45,7 +45,7 @@ public class WaitingEnemyController implements Initializable {
     Label WaitLabel;
     @FXML
     private Button BackButton;
-    public static int SWITCHER = 2;
+    public static int SWITCHER = 1;
     private static final int PORT_NUMBER = 3571;
 
     String path_sound_click = "src/main/resources/sound/button_release_sound.mp3";
