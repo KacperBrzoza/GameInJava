5f8d507c1d917d5878d78c7334c33b631bf0849f
d4krzyk <danielkrzyk44@gmail.com>
2022-09-13 21:24:44 +0200

    Poprawki estetyczne

--- src/main/java/com/example/Main/Login/StartLoginApplication.java
+++ src/main/java/com/example/Main/Login/StartLoginApplication.java
@@ -2,9 +2,11 @@ package com.example.Main.Login;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -18,17 +20,24 @@ public class StartLoginApplication extends Application
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/Main/Login/hello-login-view.fxml"));
         Parent root = loader.load();
         Scene scene = new Scene(root);
-        stage.setHeight(1920);
+        stage.setHeight(1080);
         stage.setWidth(1920);
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        //set Stage boundaries to visible bounds of the main screen
+        stage.setX(primaryScreenBounds.getMinX());
+        stage.setY(primaryScreenBounds.getMinY());
+        stage.setWidth(primaryScreenBounds.getWidth());
+        stage.setHeight(primaryScreenBounds.getHeight());
         stage.initStyle(StageStyle.UNDECORATED);
         Image icon = new Image(String.valueOf(StartLoginApplication.class.getResource("/icon/icon_KTC_og-size.png")));
         stage.getIcons().add(icon);
         stage.setTitle("Kingdoms Towers Collide");
         stage.setX(0);
         stage.setY(0);
+
         scene.getStylesheets().add(getClass().getResource("/style/style-class.css").toExternalForm());
         stage.setMaximized(true);
-        stage.setResizable(false);
+        stage.setResizable(true);
         stage.setScene(scene);
         stage.show();
     }
